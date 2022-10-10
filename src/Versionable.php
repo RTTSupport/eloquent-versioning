a74a3289deb6f39efb1c8e933c9789599cc8894b
Ryan T <24463228+RTTSupport@users.noreply.github.com>
2022-10-10 01:59:03 +0100

    Update Versionable.php

--- src/Versionable.php
+++ src/Versionable.php
@@ -2,20 +2,12 @@
 
 namespace ProAI\Versioning;
 
-use Illuminate\Support\Arr;
-use Illuminate\Support\Str;
-
-trait Versionable
+trait HasRelationships
 {
-    protected $relation = false;
+    public $relation = false;
 
-    /** Relationships */
-    public function versions() {
-        return $this->hasMany($this->getVersionTable(), 'ref_id');
-    }
-
-    /** Relationship Helpers */
-    public function asRelation($joiningTable, $pivotField) {
+    public function asRelation($joiningTable, $pivotField)
+    {
         $this->relation = ['table' => $joiningTable, 'field' => $pivotField];
 
         return $this;
@@ -51,131 +43,9 @@ trait Versionable
 
 
         return $this->newBelongsToMany(
-            $instance->asRelation($table, Str::snake(class_basename($related)).'_version')->newQuery(), $this, $table, $foreignPivotKey,
+            $instance->asRelation($table, Str::snake(class_basename($related)) . '_version')->newQuery(), $this, $table, $foreignPivotKey,
             $relatedPivotKey, $parentKey ?: $this->getKeyName(),
             $relatedKey ?: $instance->getKeyName(), $relation
         );
     }
-
-    /**
-     * Boot the versionable trait for a model.
-     *
-     * @return void
-     */
-    public static function bootVersionable()
-    {
-        static::addGlobalScope(new VersioningScope);
-    }
-
-
-    /**
-     * Create a new model instance that is existing.
-     *
-     * @param  array  $attributes
-     * @param  string|null  $connection
-     * @return static
-     */
-    public function newFromBuilder($attributes = array(), $connection = null)
-    {
-        // hide ref_id from model, because ref_id == id
-        $attributes = Arr::except((array) $attributes, $this->getVersionKeyName());
-
-        return parent::newFromBuilder($attributes, $connection);
-    }
-
-    /**
-     * Create a new Eloquent query builder for the model.
-     *
-     * @param  \Illuminate\Database\Query\Builder $query
-     * @return \ProAI\Versioning\Builder|static
-     */
-    public function newEloquentBuilder($query)
-    {
-        return new Builder($query);
-    }
-
-    /**
-     * Get the names of the attributes that are versioned.
-     *
-     * @return array
-     */
-    public function getVersionedAttributeNames()
-    {
-        return (! empty($this->versioned)) ? $this->versioned : [];
-    }
-
-    /**
-     * Get the version key name.
-     *
-     * @return string
-     */
-    public function getVersionKeyName()
-    {
-        return 'ref_' . $this->getKeyName();
-    }
-
-    /**
-     * Get the version table associated with the model.
-     *
-     * @return string
-     */
-    public function getVersionTable()
-    {
-        return $this->getTable() . '_version';
-    }
-
-    /**
-     * Get the table qualified version key name.
-     *
-     * @return string
-     */
-    public function getQualifiedVersionKeyName()
-    {
-        return $this->getVersionTable().'.'.$this->getVersionKeyName();
-    }
-
-    /**
-     * Get the name of the "latest version" column.
-     *
-     * @return string
-     */
-    public function getLatestVersionColumn()
-    {
-        return defined('static::LATEST_VERSION') ? static::LATEST_VERSION : 'latest_version';
-    }
-
-    /**
-     * Get the fully qualified "latest version" column.
-     *
-     * @return string
-     */
-    public function getQualifiedLatestVersionColumn()
-    {
-        if ($this->relation) {
-            return $this->relation['table'] . '.' . $this->relation['field'];
-        } else {
-            return $this->getTable() . '.' . $this->getLatestVersionColumn();
-        }
-    }
-
-    /**
-     * Get the name of the "version" column.
-     *
-     * @return string
-     */
-    public function getVersionColumn()
-    {
-        return defined('static::VERSION') ? static::VERSION : 'version';
-    }
-
-    /**
-     * Get the fully qualified "version" column.
-     *
-     * @return string
-     */
-    public function getQualifiedVersionColumn()
-    {
-        return $this->getVersionTable().'.'.$this->getVersionColumn();
-    }
-
 }
